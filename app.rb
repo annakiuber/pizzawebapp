eedadce47709430afaa3bf05bc347d488389f944
anna.kiuber <anna.kiuber@gmail.com>
2018-04-12 15:31:30 -0400

    saved while working hahahahah!

--- app.rb
+++ app.rb
@@ -8,38 +8,49 @@ enable :sessions
 order_hash = {}
 pizza_hash = {}
 
-
+def troubleshoot
+	puts "this is #{session[:pizza_hash]}!!!!!!!!!!!!!!!!!!!!!!*******"
+	p session[:order_hash]
+end
 get "/" do
+	troubleshoot
+	session[:final_order] = []
 	erb :index
 end
 
 
 post "/get_first_name" do
+	troubleshoot
   session[:first_name] = params[:first_name]
   puts "first name here #{session[:first_name]}!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   redirect "/pizzapageform1?="
 end
 
 get "/pizzapageform1" do
+	troubleshoot
      size = size()
 		 crust = crust()
 		 sauces = sauces()
+		 session[:pizza_hash] = {}
+		 session[:order_hash] = {}
   erb :p001pizzapage, locals:{first_name: session[:first_name], size:size , crust:crust, sauces:sauces}
 end
 
 post "/pizzapageform1" do
+	troubleshoot
 	session[:pizza_number] = params[:pizza_number]
 	session[:selectpizzasize] = params[:selectpizzasize]
 	session[:selectcrust] = params[:selectcrust]
 	session[:selectsauce] = params[:selectsauce]
-	pizza_hash["selectpizzasize"] = session[:selectpizzasize]
-	pizza_hash["selectcrust"] = session[:selectcrust]
-	puts "This is the order hash after del #{pizza_hash}"
+	session[:pizza_hash]["selectpizzasize"] = session[:selectpizzasize]
+	session[:pizza_hash]["selectcrust"] = session[:selectcrust]
+	puts "This is the order hash after del #{session[:pizza_hash]}"
 	redirect "/toppings?="
 	# need if statement for reirect if 0 pizza
 end
 
 get '/toppings' do
+	troubleshoot
 	veggies = veggies()
 	meats = meats()
   erb :p002pizzapage, locals: {veggies:veggies, meats:meats}
@@ -47,59 +58,72 @@ end
 #
 #
 post '/toppings' do
-	session[:selectveggienumber] = params[:selectveggienumber]
+	troubleshoot
   session[:selectveggies] = params[:selectveggies]
-	session[:newmeatnumber] = params[:newmeatnumber]
   session[:selectmeats] = params[:selectmeats]
 	session[:yesnoextracheese] = params[:yesnoextracheese]
-	pizza_hash["selectveggienumber"] = session[:selectveggienumber]
-	pizza_hash["newmeatnumber"] = session[:newmeatnumber]
-	pizza_hash["yesnoextracheese"] = session[:yesnoextracheese]
+	session[:pizza_hash]["meats"] = session[:selectmeats]
+	session[:pizza_hash]["veg"] = session[:selectveggies]
+	session[:pizza_hash]["yesnoextracheese"] = session[:yesnoextracheese]
+	puts """
+$$$$$$$$$$$$$$$$$$$$$$$
+#{session[:pizza_hash]}
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+	"""
 	puts "This is new veggie number #{session[:newveggienumber]}!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 		puts "This is the veggie you selected #{session[:selectveggies]}!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 		redirect "/confirmandaddpizza"
 end
 
 get '/confirmandaddpizza' do
+	troubleshoot
 	"Hello Work"
-	erb :p003pizzapage, locals: {order_hash: order_hash}
+	erb :p003pizzapage #locals: {order_hash: session[:order_hash], pizza_hash: session[:pizza_hash]}
 
 end
 post '/confirmandaddpizza' do
+	troubleshoot
 	session[:confirmpizza] = params[:confirmpizza]
 	session[:addanotherpizza] = params[:addanotherpizza]
 	puts "this is confirmpizza #{session[:confirmpizza]}"
 	puts "this is addanotherpizza #{session[:addanotherpizza]}"
 	if session[:confirmpizza] == "Yes" && session[:addanotherpizza] == "Yes"
-		redirect '/pizzapageform1'
+		puts "this is #{session[:pizza_hash]}!!!!!!!!!!!!!!!!!!!!!!*******"
+		p session[:order_hash]
+		session[:final_order] << session[:pizza_hash]
+			redirect '/pizzapageform1'
+
+
 	elsif session[:confirmpizza] == "No" && session[:addanotherpizza] == "Yes"
-		pizza_hash.delete(pizza_hash)
+		session[:pizza_hash].clear
+		p session[:order_hash]
 		redirect '/pizzapageform1'
 	elsif session[:confirmpizza] == "Yes" && session[:addanotherpizza] == "No"
-		order_hash.merge(pizza_hash)
+		session[:final_order] << session[:pizza_hash]
+		p session[:order_hash]
 		redirect '/sides'
-	else session[:confirmpizza] ++ "No" && session[:addanotherpizza] == "No"
-		pizza_hash.delete(pizza_hash)
+	elsif session[:confirmpizza] == "No" && session[:addanotherpizza] == "No"
+	session[:pizza_hash].clear
+	p session[:order_hash]
 		redirect "/"
 end
 end
 
 get '/sides' do
+	troubleshoot
 	wings = wings()
 	saladsforwebapp = saladsforwebapp()
 	erb :p004pizzapage
-
-
-
 end
 
  post '/sides' do
+	 troubleshoot
 	 session[:yesnowings] = params[:yesnowings]
 	 session[:selectwings] = params[:selectwings]
 	 session[:yesnosalad] = params[:yesnosalad]
 	 session[:selectsalads] = params[:selectsalads]
-	 order_hash["yesnowings"] = session[:yesnowings]
-	 order_hash["yesnosalad"] = session[:yesnowings]
+	 session[:order_hash]["yesnowings"] = session[:yesnowings]
+	 session[:order_hash]["yesnosalad"] = session[:yesnosalad]
 	 puts "this is what you picked: #{session[:yesnowings]},
 	  #{session[:selectwings]}, #{session[:yesnosalad]}, #{session[:selectsalads]}"
 	 redirect "/drinkanddesserts?="
@@ -108,51 +132,74 @@ end
  #need to add if/else statement for redirect fo sides.
 
  get '/drinkanddesserts' do
+	 troubleshoot
 	 erb :p005pizzapage
  end
 
  post '/drinkanddesserts' do
+	 troubleshoot
 	 session[:yesnodrinks] = params[:yesnodrinks]
 	 session[:selectdrinks] =  params[:selectdrinks]
 	 session[:yesnoicecream] = params[:yesnoicecream]
-	 order_hash["yesnodrinks"] = session[:yesnodrinks]
-	 order_hash["yesnoicecream"] = session[:yesnoicecream]
+	 session[:order_hash]["yesnodrinks"] = session[:yesnodrinks]
+	 session[:order_hash]["yesnoicecream"] = session[:yesnoicecream]
  	redirect "/deliveryform?="
 
  end
 
  get '/deliveryform' do
+	 troubleshoot
   erb :p006pizzapage
 end
 
 post '/deliveryform' do
+	troubleshoot
 	session[:yesnodelivery] = params[:yesnodelivery]
 	session[:address] = params[:address]
 	session[:yesnotip] = params[:yesnotip]
 	session[:tipamount] = params[:tipamount]
-	order_hash["yesnodelivery"] = session[:yesnodelivery]
-	order_hash["yesnotip"] = session[:yesnotip]
-	order_hash["tipamount"] = session[:tipamount]
-	redirect "/confirmationoftoppings?="
+	session[:order_hash]["yesnodelivery"] = session[:yesnodelivery]
+	session[:order_hash]["yesnotip"] = session[:yesnotip]
+	session[:order_hash]["tipamount"] = session[:tipamount]
+	redirect "/confirmfinalorder?="
 end
 
-get '/confirmationoftoppings' do
+get '/confirmfinalorder' do
+	troubleshoot
 	puts """
 	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	#{order_hash}
+	#{session[:order_hash]}
 	"""
+	session[:final_order] << session[:order_hash]
+	p "This is the final order #{session[:final_order]}"
 	# order_hash = {"yesnodelivery"=>"Yes", "yesnotip"=>"Yes", "tipamount"=>"20"}
-	erb :p007pizzapage, locals:{order_hash: order_hash, pizza_number: session[:pizza_number]}
+	erb :p007pizzapage, locals:{order_hash: session[:order_hash], final_order: session[:final_order]}
 end
 
-post '/confirmationoftoppings' do
-	redirect "/final_total_end?="
+post '/confirmfinalorder' do
+	troubleshoot
+	session[:yesnoconfirmorder] = params[:yesnoconfirmorder]
+	if session[:yesnoconfirmorder] = "yes"
+		puts "your total price is #{session[:final_order]}!!!!!!!!!!!!!!!!"
+		redirect "/final_total_end?="
+	else
+			redirect "/"
+		end
+		puts "your total price is #{session[:final_order]}!!!!!!!!!!!!!!!!"
+	end
+
+
+	get '/final_total_end' do
+		troubleshoot
+		session[:final_total] = totalpricefunction(session[:final_order])
+  "Hello World"
+	erb :p008pizzapage, locals:{final_order: session[:final_order]}
 end
 
-get '/final_total_end' do
-	pizza_number = session[:pizza_number]
-	final_total = totalpricefunction(pizza_number, order_hash)
+post '/final_total_end' do
+	troubleshoot
+	session[:final_total] = params[:final_total]
 	puts "your total price is #{session[:final_total]}!!!!!!!!!!!!!!!!"
-	erb :p008pizzapage, locals:{final_total:final_total}
+	redirect "/"
 
 end
--- totalfunction.rb
+++ totalfunction.rb
@@ -1,116 +1,108 @@
-def totalpricefunction(pizza_number, order_hash)
+def totalpricefunction(final_order)
   total_cost = 0
-  order_hash.each do |item, value|
+  before_taxes = 0
+  final_order.each do |order_hash|
+    p order_hash
+    order_hash.each_pair do |item, value|
+      p item
+      p value
 
-    pizza_number = 0
+      pizza_number = 0
 
-    sizefee = 0
-    if item == "selectpizzasize"
-      if value == "small"
-        sizefee += 0.00
-      elsif value == "medium"
-        sizefee += 1.00
-      else value == "large"
-        sizefee += 2.00
+      sizefee = 0
+      if item == "selectpizzasize"
+        if value == "small"
+          sizefee += 0.00
+        elsif value == "medium"
+          sizefee += 1.00
+        else value == "large"
+          sizefee += 2.00
+        end
+        pizza_number += 1
       end
-      pizza_number += 1
-    end
 
-    crustfee = 0
-    if item == "selectcrust"
-      if value == "skin shavings"
-        crustfee += 2.00
-      elsif value == "pan"
-        crustfee += 1.00
-      else value == "garlic"
-        crustfee += 0
+      crustfee = 0
+      if item == "selectcrust"
+        if value == "skin shavings"
+          crustfee += 2.00
+        elsif value == "pan"
+          crustfee += 1.00
+        else value == "garlic butter"
+          crustfee += 0
+        end
       end
-    end
 
-    extracheesefee = 0
-    if item == "yesnoextracheese"
-      if value == "yes"
-        extracheesefee += 1.00
-      else
-        extracheesefee += 0
+      extracheesefee = 0
+      if item == "yesnoextracheese"
+        if value == "yes"
+          extracheesefee += 1.00
+        else
+          extracheesefee += 0
+        end
       end
-    end
 
-    veggiefee = 0
-    if item == "selectveggienumber"
-      if value == 0
-        veggiefee += 0
-      elsif value == 1
-        veggiefee += 1.00
-      elsif value == 2
-        veggiefee += 2.00
-      elsif value == 3
-        veggiefee += 3.00
-      else value == 4
-        veggiefee += 4.00
+  # selectveggienumber = 0
+
+      veggiefee = 0
+      if item == "veg"
+        veggiefee = value.length.to_f
+        # selectveggienumber += value.length
       end
-    end
 
-    meatfee = 0
-    if item == "newmeatnumber"
-      if value == 0
-        meatfee += 0
-      elsif value == 1
-        meatfee += 1.00
-      elsif value == 2
-        meatfee += 2.00
-      elsif value == 3
-        meatfee += 3.00
-      else value == 4
-        meatfee += 4.00
+   # newmeatnumber = 0
+      meatfee = 0
+      if item == "meats"
+        meatfee =  value.length.to_f
+
+      # newmeatnumber += value.length
       end
-    end
 
 
 
-    wingfee = 0
-    if item == "yesnowings"
-      if value == "yes"
-        wingfee = 10.00
-      else wingfee = 0
-        wingfee += 0
+      wingfee = 10.00
+      if item == "selectwings"
+        wingfee = wingfee * selectwings.length.to_f
       end
-    end
 
-    drinkfee = 0
-    if item == "selectdrinks"
-      if value == "yes"
-        drinkfee += 3.00
-      else value =="no"
-        drinkfee += 0
-      end
-    end
 
-    icecreamfee = 0
-    if item == "yesnoicecream"
-      if value == "yes"
-        icecreamfee += 3.00
-      else value == "no"
-        icecreamfee += 0.00
+    drinkfee = 3.00
+      if item == "selectdrinks"
+        drinkfee = drinkfee * selectdrinks.length.to_f
       end
+
+    saladfee = 5.00
+    if item == "selectsalads"
+      saladfee = saladfee * selectsalads.length.to_f
     end
 
 
-    deliveryfee = 0
-    if item == "yesnodelivery"
-      if value == "yes"
-        deliveryfee += 5.00
-      else value == "no"
-        deliveryfee += 0.00
+      icecreamfee = 0
+      if item == "yesnoicecream"
+        if value == "yes"
+          icecreamfee += 3.00
+        else value == "no"
+          icecreamfee += 0.00
+        end
       end
+
+
+      deliveryfee = 0
+      if item == "yesnodelivery"
+        if value == "yes"
+          deliveryfee += 5.00
+        else value == "no"
+          deliveryfee += 0.00
+        end
+      end
+
+      cost = 10.00 * pizza_number.to_i
+      before_taxes += cost + icecreamfee + deliveryfee + crustfee + extracheesefee + veggiefee + meatfee + sizefee + wingfee + drinkfee + saladfee
+
+
     end
-
-    cost = 10.00 * pizza_number.to_i
-    tax = 0.07
-    before_taxes = cost + icecreamfee + deliveryfee + crustfee + extracheesefee + veggiefee + meatfee + sizefee + wingfee + drinkfee
-    salestax = tax * before_taxes.to_f
-    total_cost = before_taxes + tax
-
   end
+  tax = 0.07
+  salestax = tax * before_taxes.to_f
+  total_cost = before_taxes + tax
   total_cost
 end
